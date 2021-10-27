9920131dd4ad2e7a1c91d8ad380deacb945b90a4
Elisama Talita Cabral Santana <82914088+TalitaCbral@users.noreply.github.com>
2021-10-27 16:57:03 -0300

    Atualização final

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/project-crud'),
+    UserModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { User, UserSchema } from './entities/user.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  controllers: [UserController],
+  providers: [UserService],
 })
-export class UsersModule {}
+export class UserModule { }
